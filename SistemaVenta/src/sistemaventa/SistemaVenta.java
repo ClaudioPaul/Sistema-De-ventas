90572a2be0b3baa4df5149ce5bdd48c34d18896d
ClaudioPaul <claudiopaulce@gmail.com>
2022-06-08 23:00:45 -0500

    Primer Paso para el sistema nosotrs aqui Elve y el Ppupi

--- SistemaVenta/src/sistemaventa/SistemaVenta.java
+++ SistemaVenta/src/sistemaventa/SistemaVenta.java
@@ -15,7 +15,7 @@ public class SistemaVenta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
